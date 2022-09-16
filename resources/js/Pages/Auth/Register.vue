50b8bea0345a6d4d67d00dbdbe4148f8965fc09c
taftadjani <taftadjani@gmail.com>
2022-09-16 19:12:52 +0100

    Front-end Develop Register User Page - Create UI v1

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -4,6 +4,7 @@ import LaruButton from '@/Components/Laru/Button.vue';
 import LaruLink from "@/Components/Laru/Link.vue";
 import LaruInput from '@/Components/Laru/Input.vue'
 import LaruGuestLayout from '@/Layouts/Laru/Guest.vue';
+import LaruValidationErrors from '@/Components/Laru/ValidationErrors.vue';
 
 const form = useForm({
     name: '',
@@ -25,31 +26,34 @@ const submit = () => {
 
         <Head title="Register" />
 
-        <form @submit.prevent="submit" class="space-y-6">
-            <LaruInput type="text" class="block w-full" v-model="form.name"
-                placeholder="Name" />
+        <div>
+            <LaruValidationErrors class="mb-4"/>
+            <form @submit.prevent="submit" class="space-y-6">
+                <LaruInput type="text" class="block w-full" v-model="form.name"
+                    placeholder="Name" autofocus/>
 
-            <LaruInput type="email" class="block w-full"
-                v-model="form.email" placeholder="Email" />
+                <LaruInput type="email" class="block w-full"
+                    v-model="form.email" placeholder="Email" />
 
-            <LaruInput type="password" class="block w-full"
-                v-model="form.password" placeholder="Password" />
+                <LaruInput type="password" class="block w-full"
+                    v-model="form.password" placeholder="Password" />
 
-            <LaruInput type="password" class="block w-full"
-                v-model="form.password_confirmation"
-                placeholder="Password confirmation" />
+                <LaruInput type="password" class="block w-full"
+                    v-model="form.password_confirmation"
+                    placeholder="Password confirmation" />
 
-            <div class="flex items-center justify-end">
-                <LaruLink :href="route('login')">
-                    Already registered?
-                </LaruLink>
+                <div class="flex items-center justify-end">
+                    <LaruLink :href="route('login')">
+                        Already registered?
+                    </LaruLink>
 
-                <LaruButton class="ml-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing">
-                    Register
-                </LaruButton>
-            </div>
-        </form>
+                    <LaruButton class="ml-4"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing">
+                        Register
+                    </LaruButton>
+                </div>
+            </form>
+        </div>
     </LaruGuestLayout>
 </template>
